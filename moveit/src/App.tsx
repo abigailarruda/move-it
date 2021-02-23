9404bd8fb1f09e466b99f8cf3d39c7715fa451db
Abigail Arruda <itssabbie@gmail.com>
2021-02-22 22:23:50 -0300

    [NLW#4] 22/02

---
+++ moveit/src/App.tsx
@@ -0,0 +1,13 @@
+import ExperienceBar from "./components/ExperienceBar";
+
+import "./assets/styles/global.css";
+
+function App() {
+  return (
+    <div className="container">
+      <ExperienceBar />
+    </div>
+  );
+}
+
+export default App;
---
+++ moveit/src/components/ExperienceBar/index.tsx
@@ -0,0 +1,16 @@
+import "./styles.css";
+
+export default function ExperienceBar() {
+  return (
+    <header className="experience-bar">
+      <span>0 xp</span>
+      <div>
+        <div style={{ width: "50%" }}></div>
+        <span style={{ left: "50%" }} className="current-experience">
+          300 xp
+        </span>
+      </div>
+      <span>600 xp</span>
+    </header>
+  );
+}
